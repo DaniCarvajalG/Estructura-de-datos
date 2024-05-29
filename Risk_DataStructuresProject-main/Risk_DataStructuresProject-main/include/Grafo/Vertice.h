c4b2692154b4d662c6f2f92cb77f3b29e44ed885
Daniel Carvajal Gutiérrez <140833432+DaniCarvajalG@users.noreply.github.com>
2024-05-29 10:12:16 -0500

    Add files via upload

---
+++ Risk_DataStructuresProject-main/Risk_DataStructuresProject-main/include/Grafo/Vertice.h
@@ -0,0 +1,9 @@
+#ifndef VERTICE_H
+#define VERTICE_H
+
+class Vertice{
+    public:
+    int id;
+};
+
+#endif  // VERTICE_H
---
+++ Risk_DataStructuresProject-main/include/Grafo/Arista.h
@@ -0,0 +1,14 @@
+#ifndef ARISTA_H
+#define ARISTA_H
+
+class Arista{
+    public:
+    int idOrigen;
+    int idDestino;
+    int peso;
+
+    void setPeso(int peso);
+    int getPeso ();
+};
+
+#endif  // ARISTA_H
